e4802706484f3cf8ee27fb8e5d5cc7e19c0bf3f2
jingtao8a <1921056015@qq.com>
2023-11-13 06:12:10 -0500

    定义Master和Task结构体，完成makeMaster函数

--- src/mr/master.go
+++ src/mr/master.go
@@ -1,15 +1,48 @@
 package mr
 
-import "log"
+import (
+	"log"
+	"sync"
+)
+
 import "net"
 import "os"
 import "net/rpc"
 import "net/http"
 
+const (
+	MapTaskType    int8 = 0
+	ReduceTaskType int8 = 1
+)
+
+const (
+	PendingTaskStatus  int8 = 0
+	RunningTaskStatus  int8 = 1
+	CompleteTaskStatus int8 = 2
+)
+
+type Task struct {
+	taskType        int8
+	taskStaus       int8
+	taskId          int
+	inputFileNames  []string
+	outputFileNames []string
+}
+
+const (
+	MapPhase    int8 = 0
+	ReducePhase int8 = 1
+)
 
 type Master struct {
 	// Your definitions here.
-
+	nReduce                   int
+	mapTaskList               []Task
+	reduceTaskList            []Task
+	inCompleteMapTaskCount    int
+	inCompleteReduceTaskCount int
+	phase                     int8
+	mux                       sync.Mutex
 }
 
 // Your code here -- RPC handlers for the worker to call.
@@ -23,7 +56,6 @@ func (m *Master) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
-
 
 //
 // start a thread that listens for RPCs from worker.go
@@ -50,7 +82,6 @@ func (m *Master) Done() bool {
 
 	// Your code here.
 
-
 	return ret
 }
 
@@ -62,9 +93,31 @@ func (m *Master) Done() bool {
 func MakeMaster(files []string, nReduce int) *Master {
 	m := Master{}
 
-	// Your code here.
+	// Your code here
+	m.nReduce = nReduce
 
+	for i, file := range files {
+		m.mapTaskList = append(m.mapTaskList,
+			Task{
+				taskType:       MapTaskType,
+				taskStaus:      PendingTaskStatus,
+				taskId:         i,
+				inputFileNames: []string{file},
+			})
+	}
 
+	for i := 0; i < nReduce; i++ {
+		m.reduceTaskList = append(m.reduceTaskList,
+			Task{
+				taskType:  ReduceTaskType,
+				taskStaus: ReducePhase,
+				taskId:    i,
+			})
+	}
+
+	m.inCompleteMapTaskCount = len(files)
+	m.inCompleteReduceTaskCount = nReduce
+	m.phase = MapPhase
 	m.server()
 	return &m
 }
