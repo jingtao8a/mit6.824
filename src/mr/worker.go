5abc93f84f668ab072f70d72fbe1980469497d92
jingtao8a <1921056015@qq.com>
2023-11-15 02:07:22 -0500

    worker编写resolveMapTask函数，但是存在bug需要修复

--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,10 +1,16 @@
 package mr
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"time"
+)
 import "log"
 import "net/rpc"
 import "hash/fnv"
-
+import "github.com/google/uuid"
 
 //
 // Map functions return a slice of KeyValue.
@@ -24,18 +30,114 @@ func ihash(key string) int {
 	return int(h.Sum32() & 0x7fffffff)
 }
 
+func saveKV(kvs []KeyValue, file *os.File) error {
+	enc := json.NewEncoder(file)
+	for _, kv := range kvs {
+		err := enc.Encode(&kv)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func resolveMapTask(mapf func(string, string) []KeyValue, getTaskRes GetTaskResponse) ([]string, error) {
+	log.Println("resolveMapTask exec")
+	fileName := getTaskRes.task.inputFileNames[0]
+	f, err := os.Open(fileName)
+	if err != nil {
+		log.Printf("can't open file %s", fileName)
+		return nil, err
+	}
+	contents, err := ioutil.ReadAll(f)
+	kvs := mapf(fileName, string(contents))
+
+	intermediateKVs := make([][]KeyValue, getTaskRes.NReduce)
+
+	for _, kv := range kvs {
+		reduceID := ihash(kv.Key) % getTaskRes.NReduce
+		intermediateKVs[reduceID] = append(intermediateKVs[reduceID], kv)
+	}
+
+	outputPaths := []string{}
+	uid := uuid.New().String()[:8]
+	for reduceID, kvs := range intermediateKVs {
+		interFileName := fmt.Sprintf("mr-%v-%v-%v", getTaskRes.task.taskId, reduceID, uid)
+		f, err := os.Create(interFileName)
+		if err != nil {
+			log.Printf("create file %s fail", interFileName)
+			return nil, err
+		}
+		err = saveKV(kvs, f)
+		if err != nil {
+			log.Println("saveKV wrong")
+			return nil, err
+		}
+		outputPaths = append(outputPaths, interFileName)
+	}
+
+	return outputPaths, nil
+}
+
+func resolveReduceTask(reducef func(string, []string) string, getTaskRes GetTaskResponse) {
+
+}
 
 //
 // main/mrworker.go calls this function.
 //
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
-
+	log.SetFlags(log.Lshortfile | log.LstdFlags)
+	log.Println("worker start")
 	// Your worker implementation here.
+	for {
+		getTaskReq := GetTaskRequest{}
+		getTaskRes := GetTaskResponse{}
+		log.Println("getTaskRes.success", getTaskRes.success)
+		log.Println("getTaskRes.NReduce", getTaskRes.NReduce)
+		log.Println("getTaskRes.shouldExit", getTaskRes.shouldExit)
+		call("Master.GetTask", &getTaskReq, &getTaskRes)
+		log.Println("call Master.GetTask")
+		//log.Println("getTaskRes.success", getTaskRes.success)
+		//log.Println("getTaskRes.NReduce", getTaskRes.NReduce)
+		//log.Println("getTaskRes.shouldExit", getTaskRes.shouldExit)
+		if getTaskRes.shouldExit {
+			//所有task已经完成
+			log.Fatal("all task have finished, worker exit")
+		}
 
+		if !getTaskRes.success {
+			//没有获得task 等待1000 ms之后继续getTask
+			time.Sleep(1000 * time.Millisecond)
+			log.Println("didn't get task")
+			continue
+		}
+
+		completeTaskRequest := CompleteTaskRequest{
+			task: getTaskRes.task,
+		}
+		completeTaskResponse := CompleteTaskResponse{}
+		switch getTaskRes.task.taskType {
+		case MapTaskType:
+			outputPaths, err := resolveMapTask(mapf, getTaskRes)
+			if err != nil {
+				continue
+			}
+			completeTaskRequest.task.outputFileNames = outputPaths
+			break
+		case ReduceTaskType:
+			resolveReduceTask(reducef, getTaskRes)
+			break
+		default:
+			//assert never reach
+			log.Fatal("wrong taskType")
+		}
+
+		call("Master.CompleteTaskReq", &completeTaskRequest, &completeTaskResponse)
+	}
 	// uncomment to send the Example RPC to the master.
 	// CallExample()
-
 }
 
 //
