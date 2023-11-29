8378c2933f3d194e3bb4d02bb8ea88d4971b1e21
jingtao8a <1921056015@qq.com>
2023-11-29 03:21:57 -0500

    添加Log文件，包含LogEntries和LogEntry的定义

---
+++ src/raft/log.go
@@ -0,0 +1,27 @@
+package raft
+
+import "fmt"
+
+type LogEntry struct {
+	Command interface{}
+	Term    int
+}
+
+func (l *LogEntry) String() string {
+	return fmt.Sprintf("{Term:%v, Command:%v}", l.Term, l.Command)
+}
+
+type LogEntries []*LogEntry
+
+func (le *LogEntries) LastIndex() int { return len((*le)) - 1 }
+
+func (le *LogEntries) Get(i int) *LogEntry {
+	if i < 0 || i > le.LastIndex() {
+		return nil
+	}
+	return (*le)[i]
+}
+
+func (le *LogEntries) Append(entry *LogEntry) {
+	(*le) = append((*le), entry)
+}
