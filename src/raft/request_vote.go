a6aead66fb9b9980c78fc241186f37a68ffe447d
jingtao8a <1921056015@qq.com>
2023-12-01 07:04:33 -0500

    request_vote添加LastLogIndex LastLogTerm 判断

--- src/raft/request_vote.go
+++ src/raft/request_vote.go
@@ -10,8 +10,8 @@ type RequestVoteArgs struct {
 	// Your data here (2A, 2B).
 	Term         int
 	CandidateId  int
-	LastLogIndex int
-	LastLogTerm  int
+	LastLogIndex int // index of candidate's last log entry
+	LastLogTerm  int // term of candidate's last log entry
 }
 
 //
@@ -42,10 +42,16 @@ func (rf *Raft) RequestVote(args *RequestVoteArgs, reply *RequestVoteReply) {
 	}
 
 	if rf.voteFor == -1 || rf.voteFor == args.CandidateId {
-		reply.VoteGranted = true
-		rf.voteFor = args.CandidateId
+		commitIndex := rf.commitIndex
+		commitLog := rf.logs.Get(commitIndex)
+		if args.LastLogTerm > commitLog.Term ||
+			args.LastLogTerm == commitLog.Term && args.LastLogIndex >= commitIndex {
+			// candidate's log is at least up-to-date as receiver's log
+			reply.VoteGranted = true
+			rf.voteFor = args.CandidateId
+			rf.resetElectionTimeout() //重置定时器
+		}
 	}
-	rf.resetElectionTimeout() //重置定时器
 	log.Printf("[RequestVote] Finish: Server: %v, state: %v, current Term: %v, voteFor: %v, args: %+v", rf.me, rf.currentState, rf.currentTerm, rf.voteFor, args)
 	return
 }
